0d8ad8cd130831a64df8e4138f60432b6013f45f
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-12 22:19:29 -0300

    feat: adds preload endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"f1-radio-quiz/db"
 	"f1-radio-quiz/handlers"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+    db.SetupDatabase()
+
     http.HandleFunc("/api/quiz", handlers.GetQuiz)
     http.HandleFunc("/api/check-answer", handlers.CheckAnswer)
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/api/preload-f1-data", handlers.PreloadF1DataHandler)
+
+    fmt.Println("Server is running on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
