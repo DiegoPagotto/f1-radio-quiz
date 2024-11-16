f27c3904240cf45d46a3a133381435a1ebd80679
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-16 16:18:09 -0300

    feat: implements GetRandomSession and GetDriversBySession

--- backend/repositories/session_repository.go
+++ backend/repositories/session_repository.go
@@ -13,3 +13,18 @@ func GetSessionByKey(sessionKey int) (models.Session, error) {
 func CreateSession(session models.Session) error {
     return db.DB.Create(&session).Error
 }
+
+func GetRandomSession() (models.Session, error) {
+    var session models.Session
+    result := db.DB.Order("RANDOM()").First(&session)
+    return session, result.Error
+}
+
+
+func GetDriversBySession(sessionKey int) ([]models.Driver, error) {
+    var drivers []models.Driver
+    err := db.DB.Joins("JOIN driver_sessions ON driver_sessions.driver_driver_number = drivers.driver_number").
+        Where("driver_sessions.session_session_key = ?", sessionKey).
+        Find(&drivers).Error
+    return drivers, err
+}
