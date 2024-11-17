d3f1e43d8d9937f1e813aa8a9763dfbfe61cc44c
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-16 22:46:08 -0300

    feat: adds json field names for driver

--- backend/models/driver.go
+++ backend/models/driver.go
@@ -1,11 +1,12 @@
 package models
 
 type Driver struct {
-	DriverNumber int `gorm:"primaryKey;autoIncrement:false"`
-	FullName     string
-	CountryCode  string
-	TeamName     string
-	TeamColour   string
-	PictureURL   string
-	Sessions  []Session `gorm:"many2many:driver_sessions;"`
+    ID           int       `gorm:"primaryKey" json:"id"`
+    DriverNumber int       `json:"driver_number"`
+    FullName     string    `json:"full_name"`
+    CountryCode  string    `json:"country_code"`
+    TeamName     string    `json:"team_name"`
+    TeamColour   string    `json:"team_colour"`
+    PictureURL   string    `json:"picture_url"`
+    Sessions     []Session `gorm:"many2many:driver_sessions;" json:"-"`
 }
