c5fd9580165ae8c7d75289367042b221eeff92d3
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-18 22:34:24 -0300

    feat: creates type session response

--- backend/models/session.go
+++ backend/models/session.go
@@ -7,3 +7,10 @@ type Session struct {
 	SessionTitle string
 	Drivers      []Driver  `gorm:"many2many:driver_sessions;"`
 }
+
+type SessionResponse struct {
+	SessionKey   int `json:"session_key"`
+	Year         int `json:"year"`
+	Location     string `json:"location"`
+	SessionTitle string `json:"session_title"`
+}
