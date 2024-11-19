fb1d0de7a5254b405b46528c6d141f382d80f670
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-18 22:35:12 -0300

    refact: changes question session to session response

--- backend/models/quiz.go
+++ backend/models/quiz.go
@@ -1,7 +1,7 @@
 package models
 
 type QuizQuestion struct {
-	Session Session `json:"session"`
+	Session SessionResponse `json:"session"`
 	RadioURL string `json:"radio_url"`
 	Options []Driver `json:"options"`
 	AnswerDriverNumber int `json:"answer_driver_number"`
