ceea66af2abde6f0ae98600a5895618a426775da
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-22 22:55:03 -0300

    feat: adds mocked content into quiz

--- frontend/src/views/quiz/Quiz.tsx
+++ frontend/src/views/quiz/Quiz.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Layout } from 'antd';
 import './quiz.css';
+import RadioPlayer from '../../components/radio-player/RadioPlayer';
+import CircuitInfo from '../../components/circuit-info/CircuitInfo';
+import DriverCard from '../../components/driver-card/DriverCard';
 
 const { Content } = Layout;
 
@@ -8,7 +11,18 @@ const Quiz: React.FC = () => {
     return (
         <Layout className="quiz-layout">
             <Content className="quiz-content">
-                <div className="quiz-container"></div>
+                <div className="quiz-container">
+                    <div className="quiz-header">
+                        <RadioPlayer />
+                        <CircuitInfo />
+                    </div>
+                    <div className="quiz-options">
+                        <DriverCard />
+                        <DriverCard />
+                        <DriverCard />
+                        <DriverCard />
+                    </div>
+                </div>
             </Content>
         </Layout>
     );
