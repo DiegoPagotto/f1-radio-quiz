d4feac5d22cdfdde256823f47c17bc28bafdbcee
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-22 23:19:25 -0300

    feat: implements backend .env and cors liberations

--- backend/go.mod
+++ backend/go.mod
@@ -8,12 +8,15 @@ require (
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"f1-radio-quiz/db"
-	"f1-radio-quiz/handlers"
+	appHandlers "f1-radio-quiz/handlers"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	corsHandlers "github.com/gorilla/handlers"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+
+    backendPort := os.Getenv("BACKEND_PORT")
+    frontendBaseURL := os.Getenv("FRONTEND_BASE_URL")
+
     db.SetupDatabase()
 
-    http.HandleFunc("/api/quiz", handlers.GetQuiz)
-    http.HandleFunc("/api/preload-f1-data", handlers.PreloadF1DataHandler)
+    http.HandleFunc("/api/quiz", appHandlers.GetQuiz)
+    http.HandleFunc("/api/preload-f1-data", appHandlers.PreloadF1DataHandler)
 
-    fmt.Println("Server is running on http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    corsAllowedOrigins := corsHandlers.AllowedOrigins([]string{frontendBaseURL})
+    corsAllowedMethods := corsHandlers.AllowedMethods([]string{"GET", "POST"})
+    corsAllowedHeaders := corsHandlers.AllowedHeaders([]string{"Content-Type"})
+
+    fmt.Printf("Server is running on port %s\n", backendPort)
+    log.Fatal(http.ListenAndServe(":"+backendPort, corsHandlers.CORS(corsAllowedOrigins, corsAllowedMethods, corsAllowedHeaders)(http.DefaultServeMux)))
 }
