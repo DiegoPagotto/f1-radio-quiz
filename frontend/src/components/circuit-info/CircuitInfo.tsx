44b6e464046e4ea5a7883f0f252ecae05c2fcb11
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-22 22:54:27 -0300

    feat: creates initial circuitinfo component

---
+++ frontend/src/components/circuit-info/CircuitInfo.tsx
@@ -0,0 +1,10 @@
+const CircuitInfo = () => {
+    return (
+        <div>
+            <h1>Circuit Info</h1>
+            <p>Some info about the circuit</p>
+        </div>
+    );
+};
+
+export default CircuitInfo;
