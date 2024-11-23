252a7d1feb9ea8985f47c4d64e518341f74c8c0a
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-23 15:56:08 -0300

    feat: adds other country codes mapping

--- backend/utils/country_utils.go
+++ backend/utils/country_utils.go
@@ -2,6 +2,9 @@ package utils
 
 var countryCodeMap = map[string]string{
     "NED": "NLD", // Netherlands
+    "MON": "MCO", // Monaco
+    "GER": "DEU", // Germany
+    "DEN": "DNK", // Denmark
 }
 
 func SanitizeCountryCode(countryCode string) string {
