dc85e673d96afd2b14394179c04e1ba63c4ab861
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-25 22:54:13 -0300

    feat: adds circuit info component

--- frontend/src/components/circuit-info/CircuitInfo.tsx
+++ frontend/src/components/circuit-info/CircuitInfo.tsx
@@ -1,8 +1,16 @@
-const CircuitInfo = () => {
+import { Session } from '../../types/session';
+
+interface CircuitInfoProps {
+    session: Session;
+}
+
+const CircuitInfo = ({ session }: CircuitInfoProps) => {
+    console.log(session);
     return (
         <div>
-            <h1>Circuit Info</h1>
-            <p>Some info about the circuit</p>
+            <h1>{session.location}</h1>
+            <h3>{session.sessionTitle}</h3>
+            <h3>{session.year}</h3>
         </div>
     );
 };
